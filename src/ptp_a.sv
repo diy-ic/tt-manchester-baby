f2a55dd241bd8765e732ef74b1ec03f0836cdf13
kris j <kris.jurkans@gmail.com>
2024-05-16 13:50:47 +0100

    updated to only update output when pulsed

--- src/ptp_a.sv
+++ src/ptp_a.sv
@@ -6,12 +6,24 @@ module ptp_a (
     output reg [31:0] value_o
 );
 
+    reg [31:0] internal_shift_reg;
+    reg [2:0] counter;
+
     always @ (posedge control_i or posedge reset_i) begin
         
         if (reset_i) begin
             value_o <= 32'b0;
+            counter <= 3'b0;
+            internal_shift_reg <= 32'b0;
         end else begin
-            value_o <= {value_o[23:0], value_i};
+            counter <= counter + 1;
+            internal_shift_reg <= {internal_shift_reg[23:0], value_i};
+            // value_o <= {value_o[23:0], value_i};
+
+            if (counter == 3'b100) begin
+                value_o <= internal_shift_reg;
+                counter <= 3'b0;
+            end
         end
 
     end
